3da02b47437b076bd08a61da7c88b64e6ca1d513
André Costa <toscmen.com@gmail.com>
2024-02-24 01:04:31 +0100

    need to figure out a way to generate 64 bit uuid instead of 128. or maybe use 128 bits for every id

--- src/models/listener.rs
+++ src/models/listener.rs
@@ -1,6 +1,6 @@
-use std::hash::Hash;
-
 use serde::{Deserialize, Serialize};
+use std::hash::Hash;
+use uuid::Uuid;
 #[derive(Hash, Debug, Serialize, Deserialize)]
 pub struct Listener {
     pub id: u64,
@@ -9,5 +9,10 @@ pub struct Listener {
 impl Listener {
     pub fn new(id: u64) -> Listener {
         Listener { id }
+    }
+    pub fn new_with_random_uuid() -> Listener {
+        Listener {
+            id: Uuid::new_v4().as_u128(),
+        }
     }
 }
