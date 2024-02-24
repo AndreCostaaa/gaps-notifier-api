3f11f9916ba1e3b00d8a71673a183a8113a304e2
André Costa <toscmen.com@gmail.com>
2024-02-24 23:40:18 +0100

    get list of course listeners

--- src/logic/course_listener.rs
+++ src/logic/course_listener.rs
@@ -1,13 +1,23 @@
 use crate::{
     db::db::Database,
-    models::{course_listener::CourseListener, identifiable::Identifiable},
+    models::{course_listener::CourseListener, grade::Grade, identifiable::Identifiable},
 };
 
-fn list_key(course_listener: &CourseListener) -> String {
-    format!("cl_{}", course_listener.get_id())
+fn list_key(course_listener_id: u128) -> String {
+    format!("cl_{}", course_listener_id.to_string())
 }
 pub async fn register<Db: Database>(db: &mut Db, course_listener: &CourseListener) -> bool {
-    let list_key = list_key(&course_listener);
+    let list_key = list_key(course_listener.get_id());
 
     db.add_to_list(&list_key, course_listener).await
+}
+
+pub async fn get_course_listeners<Db: Database>(
+    db: &mut Db,
+    grade: &Grade,
+) -> Option<Vec<CourseListener>> {
+    let cl_id = CourseListener::compute_key(&grade.class, &grade.course, grade.year);
+
+    let list_key = list_key(cl_id.into());
+    db.fetch_list(&list_key).await
 }
