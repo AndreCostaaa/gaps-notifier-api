9f7d0dadde90b10bf9063a0356a225a65a633597
André Costa <toscmen.com@gmail.com>
2024-02-24 12:27:52 +0100

    uuids

--- src/models/listener.rs
+++ src/models/listener.rs
@@ -3,11 +3,11 @@ use std::hash::Hash;
 use uuid::Uuid;
 #[derive(Hash, Debug, Serialize, Deserialize)]
 pub struct Listener {
-    pub id: u64,
+    pub id: u128,
 }
 
 impl Listener {
-    pub fn new(id: u64) -> Listener {
+    pub fn new(id: u128) -> Listener {
         Listener { id }
     }
     pub fn new_with_random_uuid() -> Listener {
