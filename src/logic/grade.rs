cb5629b12ba1968cccc99a60a6766e37cc9defca
André Costa <toscmen.com@gmail.com>
2024-02-24 23:07:15 +0100

    added basic logic to test endpoints

--- src/logic/grade.rs
+++ src/logic/grade.rs
@@ -1,21 +1,28 @@
+use tracing::info;
+
 use super::{hashing::calculate_hash, time::current_school_year};
 use crate::db::db::Database;
 use crate::models::grade::Grade;
 
-pub fn register_grade<Db: Database>(
+static MUTEX: tokio::sync::Mutex<()> = tokio::sync::Mutex::const_new(());
+
+pub async fn register_grade<Db: Database>(
     db: &mut Db,
     course: String,
     name: String,
     class: String,
     class_average: f64,
 ) -> bool {
+    let _ = MUTEX.lock().await;
     let grade = Grade::new(course, class, current_school_year(), name, class_average);
 
-    let grade_hash = calculate_hash(&grade);
+    let grade_hash: u128 = calculate_hash(&grade).into();
 
-    if db.fetch::<Grade>(grade_hash.into()).is_some() {
+    println!("Grade hash: {}", grade_hash);
+    if db.fetch_object::<Grade>(grade_hash).await.is_some() {
+        println!("Grade already exists");
         return false;
     }
-
-    return db.save(&grade);
+    println!("Saving grade");
+    db.save_object(&grade).await
 }
