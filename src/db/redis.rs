da4bceac362bae3f21eacc6c4e71aa8ef53e3d1d
André Costa <toscmen.com@gmail.com>
2024-02-24 23:04:05 +0100

    implemented lists

--- src/db/redis.rs
+++ src/db/redis.rs
@@ -2,6 +2,7 @@ use redis::{aio::ConnectionManager, AsyncCommands, RedisResult};
 
 use crate::db::db::Database;
 use crate::logic::hashing::calculate_hash;
+use crate::models::identifiable::Identifiable;
 use std::hash::Hash;
 
 #[derive(Clone)]
@@ -14,9 +15,38 @@ impl RedisDb {
         Ok(self.connection.get(key).await?)
     }
 
-    async fn set(&mut self, key: &String, value: &String) -> RedisResult<()> {
-        Ok(self.connection.set(key, value).await?)
+    // async fn set(&mut self, key: &String, value: &String) -> RedisResult<()> {
+    //     Ok(self.connection.set(key, value).await?)
+    // }
+    async fn set_nx(&mut self, key: &String, value: &String) -> RedisResult<()> {
+        Ok(self.connection.set_nx(key, value).await?)
     }
+    async fn list_append(&mut self, key: &str, value: &str) -> RedisResult<()> {
+        Ok(self.connection.rpush(key, value).await?)
+    }
+    async fn get_list(&mut self, key: &str) -> RedisResult<Vec<String>> {
+        Ok(self.connection.lrange(key, 0, -1).await?)
+    }
+    async fn delete_key(&mut self, key: &str) -> RedisResult<()> {
+        Ok(self.connection.del(key).await?)
+    }
+    async fn delete_from_list(&mut self, list_id: &str, obj_id: &str) -> RedisResult<()> {
+        Ok(self.connection.lrem(list_id, 0, &obj_id).await?)
+    }
+    fn compute_object_key<T>(obj: &T) -> String
+    where
+        T: Hash,
+    {
+        Self::compute_obj_key::<T>(calculate_hash(obj).into())
+    }
+    fn compute_obj_key<T>(obj_id: u128) -> String {
+        format!(
+            "{}_{}",
+            std::any::type_name::<T>().split("::").last().unwrap(),
+            obj_id
+        )
+    }
+
     pub async fn new(url: &String) -> RedisDb {
         let connection_manager = redis::Client::open(url.as_str())
             .expect("Invalid connection URL")
@@ -31,27 +61,26 @@ impl RedisDb {
 }
 
 impl Database for RedisDb {
-    async fn save<T>(&mut self, obj: &T) -> bool
+    async fn save_object<T>(&mut self, obj: &T) -> bool
     where
         T: Hash + serde::Serialize,
     {
-        let type_name = std::any::type_name::<T>().split("::").last().unwrap();
-        let key = format!("{}{}", type_name, calculate_hash(obj));
+        let key = Self::compute_object_key(obj);
         let repr = self.serialize(obj);
-
         if let Some(repr) = repr {
-            match self.set(&key, &repr).await {
+            match self.set_nx(&key, &repr).await {
                 Ok(_) => return true,
                 Err(_) => return false,
             }
         }
         false
     }
-    async fn fetch<T>(&mut self, obj_id: u128) -> Option<T>
+    async fn fetch_object<T>(&mut self, obj_id: u128) -> Option<T>
     where
         T: serde::de::DeserializeOwned,
     {
-        let result = self.get(&obj_id.to_string()).await;
+        let key = Self::compute_obj_key::<T>(obj_id);
+        let result = self.get(&key).await;
 
         if let Ok(result) = result {
             self.deserialize(&result)
@@ -59,21 +88,57 @@ impl Database for RedisDb {
             None
         }
     }
-}
-fn get_uri_scheme(tls: bool) -> &'static str {
-    match tls {
-        true => "rediss",
-        false => "redis",
+
+    async fn fetch_list<T>(&mut self, list_id: &str) -> Option<Vec<T>>
+    where
+        T: serde::de::DeserializeOwned + Identifiable,
+    {
+        let result = self.get_list(list_id).await;
+
+        if let Ok(result) = result {
+            let mut vec = Vec::new();
+            for item in result {
+                if let Some(deserialized) = self.deserialize(&item) {
+                    vec.push(deserialized);
+                }
+            }
+            Some(vec)
+        } else {
+            None
+        }
     }
-}
+    async fn add_to_list<T>(&mut self, list_id: &str, value: &T) -> bool
+    where
+        T: serde::Serialize,
+    {
+        let repr = self.serialize(value);
+        if let Some(repr) = repr {
+            match self.list_append(list_id, &repr).await {
+                Ok(_) => return true,
+                Err(_) => return false,
+            }
+        }
+        false
+    }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_uri_scheme() {
-        assert_eq!(get_uri_scheme(true), "rediss");
-        assert_eq!(get_uri_scheme(false), "redis");
+    async fn delete(&mut self, key: &str) -> bool {
+        match self.delete_key(key).await {
+            Ok(_) => true,
+            Err(_) => false,
+        }
+    }
+    async fn delete_from_list<T>(&mut self, list_id: &str, obj: &T) -> bool
+    where
+        T: serde::Serialize,
+    {
+        let repr = self.serialize(&obj);
+        if let Some(repr) = repr {
+            match self.delete_from_list(&list_id, &repr).await {
+                Ok(_) => true,
+                Err(_) => false,
+            }
+        } else {
+            false
+        }
     }
 }
