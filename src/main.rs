8a66d575796c9c002e03db2152cc96935276b250
André Costa <toscmen.com@gmail.com>
2024-02-24 01:00:49 +0100

    setting up routes -- unfinished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+pub mod api;
+pub mod db;
+pub mod logic;
+pub mod models;
+use api::course_listener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .nest("/api", api::listener::routes())
+        .nest("/api", course_listener::routes())
+        .nest("/api", api::grade::routes());
+    // axum::Server::bind(&"
 }
