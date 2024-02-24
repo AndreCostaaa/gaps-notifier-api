5e8d3a1f9730b2fca93b9d3f7a148800ce646381
André Costa <toscmen.com@gmail.com>
2024-02-24 23:06:30 +0100

    post grade

--- src/api/grade.rs
+++ src/api/grade.rs
@@ -1,10 +1,10 @@
 use super::state::ApiState;
-use crate::logic::grade;
-use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Json};
+use crate::logic;
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct GradeCreateArgs {
+pub struct GradeCreateArgs {
     course: String,
     name: String,
     class: String,
@@ -12,21 +12,19 @@ struct GradeCreateArgs {
 }
 
 pub async fn create_grade(
-    State(mut apiState): State<ApiState>,
+    State(mut api_state): State<ApiState>,
     Json(body): Json<GradeCreateArgs>,
 ) -> impl IntoResponse {
-    match grade::register_grade(
-        &mut apiState.redis_db,
+    match logic::grade::register_grade(
+        &mut api_state.redis_db,
         body.course,
         body.name,
         body.class,
         body.class_average,
-    ) {
+    )
+    .await
+    {
         true => (StatusCode::CREATED, "Grade created"),
         false => (StatusCode::CONFLICT, "Grade already exists"),
     }
-}
-
-pub fn routes() -> axum::routing::Router {
-    axum::Router::new().route("/grade", post(create_grade))
 }
