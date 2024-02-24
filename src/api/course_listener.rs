dc060b335bb72b5e9c64740ac901f35c0ee3fd69
André Costa <toscmen.com@gmail.com>
2024-02-24 23:04:39 +0100

    register fn

--- src/api/course_listener.rs
+++ src/api/course_listener.rs
@@ -1,17 +1,17 @@
-use axum::{extract::State, response::IntoResponse, Json};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 
 use crate::models::course_listener::CourseListener;
 
 use super::state::ApiState;
 
-pub fn register_course_listener(
-    State(mut apiState): State<ApiState>,
-    body: Json<CourseListener>,
+pub async fn register_course_listener(
+    State(mut api_state): State<ApiState>,
+    Json(body): Json<CourseListener>,
 ) -> impl IntoResponse {
-}
-pub fn routes() -> axum::routing::Router {
-    axum::Router::new().route(
-        "/course_listener",
-        axum::routing::post(register_course_listener),
-    )
+    let course_listener =
+        crate::logic::course_listener::register(&mut api_state.redis_db, &body).await;
+    match course_listener {
+        true => (StatusCode::OK, Json(body)).into_response(),
+        false => StatusCode::CONFLICT.into_response(),
+    }
 }
