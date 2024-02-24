90979a5e4c379b0fbe6f0bf17f7ff47a3dc7954b
André Costa <toscmen.com@gmail.com>
2024-02-24 12:26:29 +0100

    created route -- unfinished

--- src/api/course_listener.rs
+++ src/api/course_listener.rs
@@ -1,1 +1,17 @@
+use axum::{extract::State, response::IntoResponse, Json};
 
+use crate::models::course_listener::CourseListener;
+
+use super::state::ApiState;
+
+pub fn register_course_listener(
+    State(mut apiState): State<ApiState>,
+    body: Json<CourseListener>,
+) -> impl IntoResponse {
+}
+pub fn routes() -> axum::routing::Router {
+    axum::Router::new().route(
+        "/course_listener",
+        axum::routing::post(register_course_listener),
+    )
+}
